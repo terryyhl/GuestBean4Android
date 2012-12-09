5cd77d10925b99f199781893b1a1e2711ffe4304
widebluesky <widebluesky@qq.com>
2012-12-10 05:21:06 +0800

    init.

---
+++ src/com/guestbean/guestbaen4android/superclass/activity/GB_BaseActivity.java
@@ -0,0 +1,7 @@
+package com.guestbean.guestbaen4android.superclass.activity;
+
+import android.app.Activity;
+
+public class GB_BaseActivity extends Activity {
+
+}
---
+++ src/com/guestbean/guestbaen4android/superclass/view/GB_TextView.java
@@ -0,0 +1,24 @@
+package com.guestbean.guestbaen4android.superclass.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class GB_TextView extends TextView {
+
+	public GB_TextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GB_TextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GB_TextView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
